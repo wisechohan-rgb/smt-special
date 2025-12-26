bae55c66efa9e20969fe33415d7f0a09f49bbf97
wisechohan-rgb <wisechohan@gmail.com>
2025-12-26 22:14:09 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -51,7 +51,7 @@ export default function Home() {
           duration: 0.5,
         }}
         className="fixed bottom-4 right-4 text-sm text-white/30 pointer-events-none z-50 font-light">
-        @dchohan.me
+        your boiii daksh
       </motion.div>
     </div>
   );
